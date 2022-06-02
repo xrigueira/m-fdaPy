12618df79fa1f38d5d26a30b3ec1b5f6b84c4c2f
Xurxo Rigueira Díaz <xrigueiradiaz@gmail.com>
2022-06-02 18:59:23 +0200

    main_v01

--- main.R
+++ main.R
@@ -1,2 +1,9 @@
-# TODO: this file will contain both algorithms from Lopez-Oriona 2021
-# or maybe I stplit it into two files...
+# TODO: this file will contain the implementation of both algorithms
+# from Lopez-Oriona 2021
+
+library(xts)
+library(dplyr)
+
+# Read the csv file
+df_csv <- read.csv("Database/argentina_pro2.csv", header = TRUE, sep = ";", stringsAsFactors = FALSE)
+print(head(df_csv))
---
+++ preprocessing.py
@@ -0,0 +1,24 @@
+
+from checkGaps import checkGaps
+from normalizer import normalizer
+from filterer import filterer
+
+
+# Define the data we want to study
+varName = 'Temperatura'
+timeFrame = 'b'
+
+
+if __name__ == '__main__':
+        
+    # Fill in the gaps in the time series
+    checkGaps(File=f'{varName}.txt')
+    print('[INFO] checkGaps() DONE')
+    
+    # Normalize the data. See normalizer.py for details
+    normalizer(File=f'{varName}_full.csv')
+    print('[INFO] normalizer() DONE')
+    
+    # Filter out those months with too many NaN and iterate on the rest
+    filterer(File=f'{varName}_nor.csv', timeframe=timeFrame)
+    print('[INFO] filterer() DONE')
